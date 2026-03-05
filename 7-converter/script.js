b64cbf60a7e574ad5368c098474b169f50af7ffb
Julia <>
2026-03-05 22:27:13 +0700

    //

--- 7-converter/script.js
+++ 7-converter/script.js
@@ -1,13 +1,13 @@
 function changeMoney (money,nameCash,needCash) {
     const ratesToDollarss = 0.012789;
     const ratesToEuro = 0.011014;
-    if (nameCash == rub && needCash == dollars) {
+    if (nameCash == 'rub' && needCash == 'dollars') {
        return money * ratesToDollarss
-    } else if (nameCash == rub && needCash == euro) {
+    } else if (nameCash == 'rub' && needCash == 'euro') {
         return money * ratesToEuro
     } else {
         return null
     }
 }
-console.log(changeMoney(1000,rub,dollars));
-console.log(changeMoney(1000,rub,euro))
+console.log(changeMoney(1000,'rub','dollars'));
+console.log(changeMoney(1000,'rub','euro'))
