4e2258f18794e7e8a34334cd2c79479a52841f70
Julia <>
2026-03-12 21:16:18 +0700

    ///

--- 8-crypto/script.js
+++ 8-crypto/script.js
@@ -1,16 +1,21 @@
-    function crypto(passw) {
-        if (typeof passw !== 'string') return null;
-        const arr = passw.split('');
-        const firstFour = arr.slice(0, 4); 
-        const lastFour = arr.slice(-4);
-        firstFour.reverse(); 
-        lastFour.reverse();  
-        return firstFour.concat(lastFour).join('');
-    }
+function crypto(passw) {
+    if (typeof passw !== 'string') return undefined;
+    
+    const firstPart = passw.slice(0, 4).split('').reverse().join('');
+    const lastPart = passw.slice(4).split('').reverse().join('');
+    
+    return firstPart + lastPart;
+}
 
-    console.log(crypto('password'));
-    function check(encrypted, original) {
-        return crypto(original) === encrypted;
-    }
+function decrypt(encrypted) {
+    return crypto(encrypted);
+}
 
-    check('password',crypto('password'))
+function check(encrypted, original) {
+    return decrypt(encrypted) === original;
+}
+
+// Тесты
+crypto('password')
+decrypt('ssapdorw');
+check('ssapdorw', 'password');
